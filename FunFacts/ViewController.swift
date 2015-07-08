a1f2cb8448e1d0f79c486c33a40508a1ef48ac71
Alp Eren Can <alperenc@gmail.com>
2015-07-09 01:22:53 +0300

    Added FactBook.swift file as the model of the app

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -12,9 +12,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var funFactLabel: UILabel!
     
+    let factBook = FactBook()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factBook.randomFact()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +26,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showFunFact() {
+        funFactLabel.text = factBook.randomFact()
     }
 
 }
