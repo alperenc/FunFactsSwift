815ba9b6d98cadac76509ffcaf366534bc803a9a
Alp Eren Can <alperenc@gmail.com>
2015-07-09 22:55:10 +0300

    Added ColorWheel.swift to randomly change background color for each fact

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -11,12 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
     
     let factBook = FactBook()
+    let colorWheel = ColorWheel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let randomColor = colorWheel.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+        
         funFactLabel.text = factBook.randomFact()
     }
 
@@ -24,8 +30,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
 
     @IBAction func showFunFact() {
+        let randomColor = colorWheel.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+        
         funFactLabel.text = factBook.randomFact()
     }
 
